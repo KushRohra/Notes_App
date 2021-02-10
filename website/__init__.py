729816cdc0b9c81acea4e2d5ec9f11acf21aea1e
Kush <krohra@cisco.com>
2021-02-10 22:38:03 +0530

    added models for databse

--- website/__init__.py
+++ website/__init__.py
@@ -1,13 +1,22 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'K'
 
+    #Database configs
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
+
     from .views import views
     from .auth import auth
 
+    # Registering Blueprints
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 

cbddbd2444d82738aff039134ad5c1425e98c2e2
Kush <krohra@cisco.com>
2021-02-10 23:09:10 +0530

    User login done and method added to prevent user from accessing the home page if not logged in

--- website/auth.py
+++ website/auth.py
@@ -2,11 +2,27 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
+from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully!', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
+            else:
+                flash('Incorrect password.', category='error')
+        else:
+            flash('Email does not exists.', category='error')
+
     return render_template("login.html")
 
 @auth.route('sign-up', methods=['GET', 'POST'])
@@ -16,8 +32,12 @@ def sign_up():
         first_name = request.form.get('firstName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
-        
-        if len(email) < 4:
+
+        user = User.query.filter_by(email=email).first()
+
+        if user:
+            flash('Email already exists.', category='error')
+        elif len(email) < 4:
             flash("Email must be greater than 3 characters.", category='error')
         elif len(first_name) < 2:
             flash("First Name must be greater than 1 characters.", category='error')
@@ -30,6 +50,7 @@ def sign_up():
             new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1, method='sha256'))
             db.session.add(new_user)
             db.session.commit()
+            login_user(user, remember=True)
             flash("Account created", category='success')
 
             return redirect(url_for('views.home'))
@@ -37,5 +58,7 @@ def sign_up():
     return render_template("sign_up.html")
 
 @auth.route('logout')
+@login_required
 def logout():
-    return "<p>logout</p>"
+    logout_user()
+    return redirect(url_for('auth.login'))
